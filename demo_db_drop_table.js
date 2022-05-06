2981212a1642f71e091e7c5d11a5358f132fdf27
ykcarlla <ykcarlla.thais@gmail.com>
2022-05-06 11:09:37 -0300

    1.0

---
+++ demo_db_drop_table.js
@@ -0,0 +1,20 @@
+
+Delete the table "customers":
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "aluo01",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "DROP TABLE customers";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table deleted");
+  });
+});
